aaf199f32b9d0b60bbfa080b4d559d5aa7d85c79
tevawolf <tevawolf@yahoo.co.jp>
2018-12-05 17:51:51 +0900

    ・文字装飾もできた →画面側に入力補助を
    【未】
    ・アンカー
    ・フィルタ
    ・ファイル添付

--- WGB/character_decoration.py
+++ WGB/character_decoration.py
@@ -3,12 +3,12 @@ import re
 
 class CharacterDecoration:
 
-    big = '%b'
-    small = '%s'
-    bold = '%bl'
-    strike = '%st'
-    color = '%c'
-    regex = r'[^\x01-\x7E]*[a-zA-Z]*[0-9]*[ｦ-ﾟ]*[ -/:-@\[-~]*'
+    BIG = '%b'
+    SMALL = '%s'
+    BOLD = '%bl'
+    STRIKE = '%st'
+    COLOR = '%c'
+    REGEX = r'.*?'
 
     def decorate(self, text):
         text = self.decorate_bold(text)
@@ -19,45 +19,45 @@ class CharacterDecoration:
         return text
 
     def decorate_big(self, text):
-        match = re.search(self.big + self.regex + self.big, text)
+        match = re.search(self.BIG + self.REGEX + self.BIG, text)
         while match:
-            t = match.group().replace(self.big, '<span style=\"font-size: x-large;\">', 1)
-            t = t.replace(self.big, '</span>', 1)
+            t = match.group().replace(self.BIG, '<span style=\"font-size: x-large;\">', 1)
+            t = t.replace(self.BIG, '</span>', 1)
             text = text.replace(match.group(), t, 1)
-            match = re.search(self.big + self.regex + self.big, text)
+            match = re.search(self.BIG + self.REGEX + self.BIG, text)
         return text
 
     def decorate_small(self, text):
-        match = re.search(self.small + self.regex + self.small, text)
+        match = re.search(self.SMALL + self.REGEX + self.SMALL, text)
         while match:
-            t = match.group().replace(self.small, '<span style=\"font-size: x-small;\">', 1)
-            t = t.replace(self.small, '</span>', 1)
+            t = match.group().replace(self.SMALL, '<span style=\"font-size: x-small;\">', 1)
+            t = t.replace(self.SMALL, '</span>', 1)
             text = text.replace(match.group(), t, 1)
-            match = re.search(self.small + self.regex + self.small, text)
+            match = re.search(self.SMALL + self.REGEX + self.SMALL, text)
         return text
 
     def decorate_bold(self, text):
-        match = re.search(self.bold + self.regex + self.bold, text)
+        match = re.search(self.BOLD + self.REGEX + self.BOLD, text)
         while match:
-            t = match.group().replace(self.bold, '<span style=\"font-weight: bold;\">', 1)
-            t = t.replace(self.bold, '</span>', 1)
+            t = match.group().replace(self.BOLD, '<span style=\"font-weight: bold;\">', 1)
+            t = t.replace(self.BOLD, '</span>', 1)
             text = text.replace(match.group(), t, 1)
-            match = re.search(self.bold + self.regex + self.bold, text)
+            match = re.search(self.BOLD + self.REGEX + self.BOLD, text)
         return text
 
     def decorate_strike(self, text):
-        match = re.search(self.strike + self.regex + self.strike, text)
+        match = re.search(self.STRIKE + self.REGEX + self.STRIKE, text)
         while match:
-            t = match.group().replace(self.strike, '<span style=\"text-decoration:line-through;\">', 1)
-            t = t.replace(self.strike, '</span>', 1)
+            t = match.group().replace(self.STRIKE, '<span style=\"text-decoration:line-through;\">', 1)
+            t = t.replace(self.STRIKE, '</span>', 1)
             text = text.replace(match.group(), t, 1)
-            match = re.search(self.strike + self.regex + self.strike, text)
+            match = re.search(self.STRIKE + self.REGEX + self.STRIKE, text)
         return text
 
     def decorate_color(self, text):
-        code_regex = r'\[#?[a-zA-Z]*[0-9]*\]'
+        code_regex = r'\[#?[a-zA-Z0-9]*\]'
 
-        match = re.search(code_regex + self.color + self.regex + self.color, text)
+        match = re.search(code_regex + self.COLOR + self.REGEX + self.COLOR, text)
         while match:
             color = re.search(code_regex, match.group())
             if not color:
@@ -65,11 +65,11 @@ class CharacterDecoration:
 
             color_code = color.group().replace('[', '').replace(']', '')
 
-            t = match.group().replace(self.color, '<span style=\"color:{};\">'.format(color_code), 1)
+            t = match.group().replace(self.COLOR, '<span style=\"color:{};\">'.format(color_code), 1)
             t = t.replace(color.group(), '', 1)
-            t = t.replace(self.color, '</span>', 1)
+            t = t.replace(self.COLOR, '</span>', 1)
             text = text.replace(match.group(), t, 1)
-            match = re.search(self.color + self.regex + self.color, text)
+            match = re.search(self.COLOR + self.REGEX + self.COLOR, text)
         return text
 
 

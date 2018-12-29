b3ed3ffbef150aeb9efe6be1def3f3e7c3ec5955
tevawolf <tevawolf@yahoo.co.jp>
2018-12-30 01:21:08 +0900

    settings.pyから完全にMySQL設定を変更

--- config/settings.py
+++ config/settings.py
@@ -78,14 +78,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wgb',
-        'USER': 'tevasaki',
-        'PASSWORD': 'teVa0210',
-        'ATOMIC_REQUESTS': True,
-        'OPTIONS': {
-          'autocommit': False,
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 

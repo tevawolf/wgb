f7a4ffe02bcc2780b6f251b13799eedacbb98c2e
tevawolf <tevawolf@yahoo.co.jp>
2019-01-06 06:19:19 +0900

    djangogirlsに合わせ（第一弾。django-herokuの使用は継続）

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,10 +25,9 @@ PROJECT_NAME = os.path.basename(BASE_DIR)
 SECRET_KEY = 'f+@s@il+@og6w_d*q0%3tuwal^3-k)7a9-3p@njlb$($aisuzg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,10 +77,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 # Password validation
